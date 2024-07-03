ebef4bab7c4f6ad88d73fce0a2c9f8a7b563faed
ofjxl <165612614+ofjxl@users.noreply.github.com>
2024-07-03 12:40:32 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -6,3 +6,11 @@ const main_s = new Swiper(".main-slide",{
         prevEl: ".arrr .swiper-button-prev",
     },
 })
+const product_s = new Swiper(".product-slide",{
+    slidesPerView:3,
+    spaceBetween:44,
+    /* navigation:{
+        nextEl: ".product-slide .swiper-button-next",
+        prevEl: ".product-slide .swiper-butto-prev",
+    }, */
+})
